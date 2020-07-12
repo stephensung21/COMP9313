97a99d4c75f6d5050569de00c1ecd21ba84ce232
stephen.j.sung@gmail.com <stephen.j.sung@gmail.com>
2020-07-13 03:23:38 +1000

    COMPLETED FIRST

---
+++ COMP9313_project_1_v1/submission.py
@@ -0,0 +1,15 @@
+## import modules here
+
+########## Question 1 ##########
+# do not change the heading of the function
+def c2lsh(data_hashes, query_hashes, alpha_m, beta_n):
+    cand = sc.emptyRDD()
+    print('dsad')
+    return cand
+
+#hashes_1 = data, hashes_2 = query
+def count(hashes_1, hashes_2, offset):
+    pass
+
+def work():
+    print(asd)
